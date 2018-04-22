2e2a6be00cc20db85b1f838441649962b5a1220a
Your Name <you@example.com>
2018-04-23 00:13:28 +0200

    homework_2

--- Lessons/Lesson2/Homework/Homework_2.R
+++ Lessons/Lesson2/Homework/Homework_2.R
@@ -2,33 +2,39 @@
 # show it on the chart using `ggplot2` package. Write an explanation about your findings into the code as comment.
 # __Commit__ it to your repository into `Lessons/Lesson2/Homework`.
 
-## Code
-# 
-# 
-# 
-# 
-# 
-# 
-# 
-# 
-# 
-# 
-# 
-# 
-# 
-# 
-# 
-# 
-# 
-# 
-# 
-# 
-# 
+# libraries
+library(dplyr)
+library(ggplot2)
+
+# load data
+dt_KPI_raw <- read.csv("C:/Users/PC/Documents/GeneralInsurance_Class/data/lesson2_KPI.csv")
+
+dt_KPI_raw <- dt_KPI_raw %>% filter(!is.na(Premium))
+for(i in 1:length(dt_KPI_raw$Premium))
+{
+  if(dt_KPI_raw$Premium[i]<0) 
+  {
+    dt_KPI_raw$Premium[i]=0
+  }
+}
+
+
+dt_KPI_raw %>%  mutate(uwr=Premium-Expenses-Losses) %>% 
+  group_by(Year) %>% 
+  summarize(uwr = sum(uwr, na.rm = TRUE)) %>%arrange(uwr)
+
+dt_KPI_raw %>% 
+  mutate(uwr=Premium-Expenses-Losses) %>% 
+  group_by(Year) %>% 
+  summarize(uwr = sum(uwr, na.rm = TRUE)) %>% 
+  ggplot(aes(x = reorder(Year, uwr), y = uwr)) + 
+  geom_col()
 
 
 
 
 # Your Explanation about analysis:
 # 
+#najhorsi rok bol 2015 lebo ma najnizsie uwr
 # 
 # 
