5dbf561300f5a389fc9de2ba6d9211ce273d77f2
Your Name <you@example.com>
2018-04-29 14:19:18 +0200

    Homework_ui

--- Lessons/Lesson1/Homework/server.R
+++ Lessons/Lesson1/Homework/server.R
@@ -1,6 +1,16 @@
+library(shiny)
+library(ggplot2)
 
+dt_KPI<-read.csv("C:/Users/PC/Documents/GeneralInsurance_Class/data/lesson2_KPI.csv")
 
 # Define a server for the Shiny app
-function(input, output) {
+server<-function(input, output) {
+  output$picture<-renderPlot( 
+  ggplot(data = dt_KPI,
+         mapping = aes_string(x = "Premium", y = "Expenses", 
+         colour = input$selected_variable)) +
+  geom_point() +
+  geom_smooth()
+                            ) 
 
-}
+                        }
--- Lessons/Lesson1/Homework/ui.R
+++ Lessons/Lesson1/Homework/ui.R
@@ -1,21 +1,22 @@
 
-
+dt_KPI<-read.csv("C:/Users/PC/Documents/GeneralInsurance_Class/data/lesson2_KPI.csv")
+variable<-c("Region", "Unit","Segment","Business","Year")
 # Use a fluid Bootstrap layout
-fluidPage(    
+ui<-fluidPage(    
   
   # Give the page a title
   titlePanel("Lesson 1 - Homework"),
-  
-
   sidebarLayout(      
     
-
     sidebarPanel(
-
+      selectInput(inputId = "selected_variable",
+                  label ="Colouring Var:",
+                  choices=variable
+                  )
     ),
     
-    
     mainPanel(
+      plotOutput("picture")
        
     )
     
